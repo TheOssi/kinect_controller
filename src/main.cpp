b1b9389d56b1cfda4fa89431022a24038a709960
TheOssi <lenk.max@googlemail.com>
2017-04-03 20:11:36 +0200

    added Joints

--- src/main.cpp
+++ src/main.cpp
@@ -5,85 +5,134 @@
 
 using namespace std;
 
-struct point{
- float x;
- float y;
- float z;
+struct point {
+	float x;
+	float y;
+	float z;
 };
 
+struct jointType {
+	enum {
+		rightHand = 0,
+		rightElbow = 1,
+		rightShoulder = 2,
+		torso = 3,
+		neck = 4,
+		head = 5,
+		rightHip = 6,
+		rightFoot = 7,
+		rightKnee = 8,
+		leftHip = 9,
+		leftKnee = 10,
+		leftFoot = 11,
+		leftShoulder = 12,
+		leftElbow = 13,
+		leftHand = 14
+	};
+};
+jointType jType;
 
-
-class SkeletonPoints
-{
+class SkeletonPoints {
 private:
-    static bool instanceFlag;
-    static SkeletonPoints *single;
-    point rightHand;
-    SkeletonPoints()
-    {
-        //private constructor
-    }
-public:
-    static SkeletonPoints* getInstance();
-    point getRightHand();
-    void setRightHand(float,float,float);
+	static bool instanceFlag;
+	static SkeletonPoints *single;
+	point points[14];
 
-    ~SkeletonPoints()
-    {
-        instanceFlag = false;
-    }
+	SkeletonPoints() {
+		//private constructor
+	}
+public:
+	static SkeletonPoints* getInstance();
+
+	void setJoint(int, float, float, float);
+	point getJoint(int);
+
+	~SkeletonPoints() {
+		instanceFlag = false;
+	}
 };
 
 bool SkeletonPoints::instanceFlag = false;
 SkeletonPoints* SkeletonPoints::single = NULL;
-SkeletonPoints* SkeletonPoints::getInstance()
-{
-    if(! instanceFlag)
-    {
-        single = new SkeletonPoints();
-        instanceFlag = true;
-        return single;
-    }
-    else
-    {
-        return single;
-    }
+SkeletonPoints* SkeletonPoints::getInstance() {
+	if (!instanceFlag) {
+		single = new SkeletonPoints();
+		instanceFlag = true;
+		return single;
+	} else {
+		return single;
+	}
 }
 
-
-
-
-point SkeletonPoints::getRightHand(){
-  return rightHand;
+void SkeletonPoints::setJoint(int joint, float x, float y, float z) {
+	points[joint].x = x;
+	points[joint].y = y;
+	points[joint].z = z;
+}
+point SkeletonPoints::getJoint(int joint) {
+	return points[joint];
 }
 
-void SkeletonPoints::setRightHand(float x, float y,float z){
-  rightHand.x = x;
-  rightHand.y = y;
-  rightHand.z = z;
+void messageCallback(const tf2_msgs::TFMessage::ConstPtr& msg) {
+	SkeletonPoints* points;
+	points = SkeletonPoints::getInstance();
+	float x = msg->transforms[0].transform.translation.x;
+	float y = msg->transforms[0].transform.translation.y;
+	float z = msg->transforms[0].transform.translation.z;
+	string childframe(msg->transforms[0].child_frame_id.c_str());
+
+	//RIGHT HAND
+	if (childframe == "right_hand_1") {
+		points->setJoint(jType.rightHand, x, y, z);
+		//RIGHT ELBOW
+	} else if (childframe == "right_elbow_1") {
+		points->setJoint(jType.rightElbow, x, y, z);
+		//RIGHT SHOULDER
+	} else if (childframe == "right_shoulder_1") {
+		points->setJoint(jType.rightShoulder, x, y, z);
+		//RIGHT HIP
+	} else if (childframe == "right_hip_1") {
+		points->setJoint(jType.rightHip, x, y, z);
+		//RIGHT KNEE
+	} else if (childframe == "right_knee_1") {
+		points->setJoint(jType.rightElbow, x, y, z);
+		//RIGHT FOOT
+	} else if (childframe == "right_foot_1") {
+		points->setJoint(jType.rightElbow, x, y, z);
+		//LEFT HAND
+	} else if (childframe == "left_hand_1") {
+		points->setJoint(jType.leftHand, x, y, z);
+		//LEFT ELBOW
+	} else if (childframe == "left_elbow_1") {
+		points->setJoint(jType.leftElbow, x, y, z);
+		//LEFT SHOULDER
+	} else if (childframe == "left_shoulder_1") {
+		points->setJoint(jType.leftShoulder, x, y, z);
+		//LEFT HIP
+	} else if (childframe == "left_hip_1") {
+		points->setJoint(jType.leftHip, x, y, z);
+		//LEFT KNEE
+	} else if (childframe == "left_knee_1") {
+		points->setJoint(jType.leftElbow, x, y, z);
+		//LEFT FOOT
+	} else if (childframe == "left_foot_1") {
+		points->setJoint(jType.leftElbow, x, y, z);
+	}
+	//TORSO
+	else if (childframe == "torso_1") {
+		points->setJoint(jType.torso, x, y, z);
+	}
+	//NECK
+	else if (childframe == "neck_1") {
+		points->setJoint(jType.neck, x, y, z);
+	}
+	//HEAD
+	else if (childframe == "head_1") {
+		points->setJoint(jType.head, x, y, z);
+	}
 }
 
-void messageCallback(const tf2_msgs::TFMessage::ConstPtr& msg)
-{
-  SkeletonPoints* points;
-  points = SkeletonPoints::getInstance();
-  float x = msg->transforms[0].transform.translation.x;
-  float y =msg->transforms[0].transform.translation.y;
-  float z = msg->transforms[0].transform.translation.z;
-  string childframe(msg->transforms[0].child_frame_id.c_str());
-  if(childframe == "right_hand_1"){
-	  points->setRightHand(x,y,z);
-	  point rHand = points->getRightHand();
-	  ROS_INFO("%6.4lf | %6.4lf | %6.4lf", rHand.x, rHand.y,rHand.z);
-  }
-
-
-}
-
-
-
-int main(int argc, char **argv)
-{
+int main(int argc, char **argv) {
 
 	ros::init(argc, argv, "listener");
 	ros::NodeHandle n;
@@ -92,7 +141,6 @@ int main(int argc, char **argv)
 
 	ros::spin();
 
-  return 0;
+	return 0;
 }
-
 
